0da581058f27340d01e42d86f313dd7f6dcc4d3f
Erik Faane <faane@hsnews.no>
2026-05-12 16:00:51 +0200

    Trimmed down redundant styling and fixed a build warning message for the guard that handles the local storage.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,14 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) {
+    return router.createUrlTree(['/login']);
+  }
 
   const user = localStorage.getItem('putteputtern_logged_in_user');
 
@@ -10,6 +16,5 @@ export const authGuard: CanActivateFn = () => {
     return true;
   }
 
-  router.navigate(['/login']);
-  return false;
+  return router.createUrlTree(['/login']);
 };
